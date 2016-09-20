ed8264f0fddc08a9337434c8c219ec05a3fd769b
Mark24 <mark.zhangyoung@qq.com>
2016-09-20 13:23:34 +0800

    vue-router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import VueValidator from 'vue-validator';
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
 import app from './app.vue';
 
 Vue.use(VueValidator);
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
 var vm = new Vue({
     el:'body',
     components:{
